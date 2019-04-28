d67c4fa6b5b8c47fa148477da2b5fb20539c179c
newwai130 <newwai130@gmail.com>
2019-04-28 16:47:57 +0800

    0.1.1

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import threading
 import time
 import queue
+import csv
+import pymysql
+import traceback
 
 # 子執行緒類別
 class Worker(threading.Thread):
@@ -25,9 +28,57 @@ def main():
 	my_queue = queue.Queue()
 
 	# 將資料放入佇列
-	for i in range(10):
-	  my_queue.put("Data %d" % i)
+	with open('Ceo.csv', newline='') as csvfile:
 
+
+		# 讀取 CSV 檔案內容
+		rows = csv.reader(csvfile)
+		next(rows, None)
+
+		# 以迴圈輸出每一列
+		for row in rows:
+			my_queue.put(row)
+
+	db = pymysql.connect("192.168.1.58", "wai", "123", "mm", charset='utf8' )
+	cursor = db.cursor()
+
+	i=0
+	try:
+		while not my_queue.empty():
+			i+=1
+			ceo_row = my_queue.get()
+			ceo_year = ceo_row[1]
+			ceo_name = ceo_row[4]
+			ceo_company = ceo_row[7]
+
+			ceo_name = ceo_name.replace("\'", "\'\'")
+			ceo_company = ceo_company.replace("\'", "\'\'")
+
+			sql = 'SELECT cname, namefmac, compustatname, Acronym from Company WHERE (cname = \"'+ ceo_company+'\" OR namefmac = \"'+ ceo_company +'\" OR compustatname = \"'+ ceo_company+'\" OR Acronym = \"'+ ceo_company+'\") AND (year='+ ceo_year+');';
+			#print(sql)
+
+			cursor.execute(sql)
+			results = cursor.fetchall()
+
+			if(i%5000==0):
+				print(i)
+
+			if(len(results) > 0):
+				print(ceo_company)
+				print(results)
+				input()
+			"""
+			for row in results:
+				fname = row[0]
+				lname = row[1]
+				age = row[2]
+				sex = row[3]
+				income = row[4]
+				print(fname)
+			"""
+	except Exception:
+		traceback.print_exc()
+	"""
 	threads = []
 	for i in range(5):
 	  threads.append(Worker(my_queue,i))
@@ -41,5 +92,5 @@ def main():
 	  threads[i].join()
 
 	print("Done.")
-
+	"""
 main()
